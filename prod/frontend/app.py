6b5f7b560ac829ce07b3c84c3d8d0bc39ace0c4a
anilatitus99 <anilatitus99@gmail.com>
2025-06-18 20:03:07 -0400

    Delete prod directory

--- prod/frontend/app.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return "<h1>Frontend - PROD</h1><p>This is the UI for the PROD environment.</p>"
--- prod/terraform/main.tf
+++
@@ -1,8 +0,0 @@
-provider "google" {
-  project = var.project_id
-  region  = var.region
-}
-
-resource "google_app_engine_application" "app" {
-  location_id = var.region
-}
