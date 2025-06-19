0f742ebb272a13d6737c07b425dac648dec66711
anilatitus99 <anilatitus99@gmail.com>
2025-06-18 20:02:53 -0400

    Delete beta directory

--- beta/frontend/app.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return "<h1>Frontend - BETA</h1><p>This is the UI for the BETA environment.</p>"
--- beta/terraform/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "app_engine_region" {
-  value = google_app_engine_application.app.location_id
-}
